4ea0a793fc59fd60025665bab21616641023560f
Matty Hana <matty.hana@gmail.com>
2023-08-10 00:33:58 +0200

    made code nicer

--- Img to value/Program.cs
+++ Img to value/Program.cs
@@ -11,13 +11,6 @@ namespace Img_to_value
 {
     internal class Program
     {
-        public static void Color(string message, ConsoleColor color)
-        {
-            Console.ForegroundColor = color;
-            Console.WriteLine(message);
-            Console.ResetColor();
-        }
-
         static void Main(string[] args)
         {
             List<int> alphaList = new List<int>();
@@ -27,14 +20,17 @@ namespace Img_to_value
             int ImgNumber = int.Parse(Console.ReadLine());
             Console.WriteLine("Please enter the file extencion");
             string extencion = Console.ReadLine();
+            Console.WriteLine("Please enter the output destinacion");
+            string outputDir = Console.ReadLine();
+
+
+
             for (int CurentImg = 1; CurentImg <= ImgNumber; CurentImg++)
             {
                 Bitmap image = new Bitmap($"{path}{CurentImg}{extencion}");
 
                 int width = image.Width;
                 int height = image.Height;
-                int white = 0;
-                int black = 0;
                 Color pixelColor = image.GetPixel(1, 1);
 
                 for (int h = 0; h < height; h++)
@@ -42,21 +38,16 @@ namespace Img_to_value
                     for (int w = 0; w < width; w++)
                     {
                         pixelColor = image.GetPixel(w, h);
-                        int red = pixelColor.R; //for fun not acualy used
+                        int red = pixelColor.R;
                         int green = pixelColor.G;
                         int blue = pixelColor.B;
                         int alpha = pixelColor.A;
-                        //Console.WriteLine($"Pixel at ({w},{h}): R={red}, G={green}, B={blue}, A={alpha}");
                         alphaList.Add(red);
-                        //Console.WriteLine(alpha + " " + red + " " + green + " " + blue);
-                        if (red == 0 || blue == 0 || green == 0)
-                        {
-                            black++;
-                        } else white++;
+                        //Console.WriteLine($"Pixel at ({w},{h}): R={red}, G={green}, B={blue}, A={alpha}");
                     }
                 }
-                string directoryPath = @"c:\Users\matty\Source\repos\.bin\";
-                string filePath = Path.Combine(directoryPath, $"output{CurentImg}.txt");
+                
+                string filePath = Path.Combine(outputDir, $"output{CurentImg}.txt");
                 using (StreamWriter writer = new StreamWriter(filePath))
                 {
                     foreach (int a in alphaList)
@@ -71,11 +62,7 @@ namespace Img_to_value
                 alphaList.Clear();
                 image.Dispose();
             }
-            //Console.WriteLine("-------------------------");
-            //Console.WriteLine("White: " + white);
-            //Console.WriteLine("Black: " + black);
-            //Console.WriteLine("* " + height * width);
-            Console.WriteLine("Finished");
+            Console.WriteLine("Press any key to exit");
             Console.ReadKey();
 
         }
